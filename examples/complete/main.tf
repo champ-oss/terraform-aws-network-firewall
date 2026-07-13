b8a8b38bbd51ccc0385ab6656deabbffcc1a0aeb
neena <neena.deshpande@champtitles.com>
2026-07-13 15:16:22 -0500

    Updated files

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "this" {
-  source                    = "https://github.com/champ-oss/terraform-aws-vpc.git?ref=v1.0.63-722aa5b"
+module "networkd-fw-vpc" {
+  source                    = "github.com/champ-oss/terraform-aws-vpc.git?ref=v1.0.63-722aa5b"
   name                      = "network-firewall-vpc"
   cidr_block                = "10.1.0.0/16"
   availability_zones_count  = 1
